ba4673a7a6ab71324993cace4420b3b3fc896a08
Edwin Cardenas <eacardenase@unal.edu.co>
2023-05-01 22:21:09 -0500

    feat: add cli to guessing game

--- src/main/com/eacardenase/game/GuessingGame.java
+++ src/main/com/eacardenase/game/GuessingGame.java
@@ -32,4 +32,27 @@ public class GuessingGame {
     public int getRandomNumber() {
         return randomNumber;
     }
+
+    public static void main(String[] args) {
+        GuessingGame game = new GuessingGame();
+        boolean loopShouldContinue = true;
+
+        do {
+            String input = System.console().readLine("Enter a number: ");
+
+            if (input.equals("q")) {
+                return;
+            }
+
+            String output = game.guess(Integer.parseInt(input));
+
+            System.out.println(output);
+
+            if (output.contains("You got it") || output.contains("over")) {
+                loopShouldContinue = false;
+            }
+        } while (loopShouldContinue);
+
+
+    }
 }
